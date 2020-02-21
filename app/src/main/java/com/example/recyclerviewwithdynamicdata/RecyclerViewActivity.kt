4cc30ccf129497db9917eafcdc8ac960d934a9dc
Singhai, Vikas <vikas.singhai@accenture.com>
2020-02-21 21:26:35 +0530

    Base code set with target SDK 14 [4.0]

---
+++ app/src/main/java/com/example/recyclerviewwithdynamicdata/RecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewwithdynamicdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.recycle_view_layout)
+    }
+}
